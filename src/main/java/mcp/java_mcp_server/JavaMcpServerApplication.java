dc69a0eab14015eba9413dfba89f9e4994ee7120
seonghoo1217 <lsh6451217@gmail.com>
2025-06-03 17:48:36 +0900

    Initial commit

---
+++ src/main/java/mcp/java_mcp_server/JavaMcpServerApplication.java
@@ -0,0 +1,13 @@
+package mcp.java_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mcp/java_mcp_server/JavaMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mcp.java_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
